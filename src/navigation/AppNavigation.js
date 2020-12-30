b49027ad776e7242b9112d256cfdb44147901f4d
SDen4 <asw325@yandex.ru>
2020-12-30 23:44:04 +0500

    test button in header

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,27 +1,34 @@
 import React from 'react';
+import { Button, Alert } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import { MainScreen } from '../screens/MainScreen';
 import { AboutScreen } from '../screens/AboutScreen';
 import { ListScreen } from '../screens/ListScreen';
-
+import { THEME } from '../theme';
 
 const Stack = createStackNavigator();
-
-const screenParams = {
-    options: {
-
-    }
-}
 
 export const AppNavigation = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name='Main' component={MainScreen} />
-                <Stack.Screen name='About' component={AboutScreen} />
-                <Stack.Screen name='List' component={ListScreen} />
+                <Stack.Screen
+                    name="Main"
+                    component={MainScreen}
+                    options={{
+                        headerRight: () => (
+                            <Button
+                                title="test button"
+                                onPress={() => Alert.alert('Test Button')}
+                                color={THEME.DANGER_COLOR}
+                            />
+                        ),
+                    }}
+                />
+                <Stack.Screen name="About" component={AboutScreen} />
+                <Stack.Screen name="List" component={ListScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
