6ee7d0cb1aa4e6330630dc1f57348161731ed34b
SDen4 <asw325@yandex.ru>
2021-01-05 00:14:43 +0500

    stack + drawer navigators

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -1,21 +1,63 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createStackNavigator } from '@react-navigation/stack';
 
 import { MainScreen } from '../screens/MainScreen';
 import { AboutScreen } from '../screens/AboutScreen';
 import { ListScreen } from '../screens/ListScreen';
+import { THEME } from '../theme';
 
 const Drawer = createDrawerNavigator();
+const Stack = createStackNavigator();
+
+const stackOptions = {
+    headerStyle: {
+        backgroundColor: THEME.MAIN_COLOR,
+    },
+    headerTintColor: '#fff',
+};
+
+const MainNavigator = () => {
+    return (
+        <Stack.Navigator screenOptions={stackOptions}>
+            <Stack.Screen name="Main" component={MainScreen} />
+            <Stack.Screen name="List" component={ListScreen} />
+            <Stack.Screen name="About" component={AboutScreen} />
+        </Stack.Navigator>
+    );
+};
+
+const ListNavigator = () => {
+    return (
+        <Stack.Navigator screenOptions={stackOptions}>
+            <Stack.Screen name="List" component={ListScreen} />
+        </Stack.Navigator>
+    );
+};
+
+const AboutNavigator = () => {
+    return (
+        <Stack.Navigator screenOptions={stackOptions}>
+            <Stack.Screen name="About" component={AboutScreen} />
+        </Stack.Navigator>
+    );
+};
+
+const DrawerNavigator = () => {
+    return (
+        <Drawer.Navigator>
+            <Drawer.Screen name="Main" component={MainNavigator} />
+            <Drawer.Screen name="List" component={ListNavigator} />
+            <Drawer.Screen name="About" component={AboutNavigator} />
+        </Drawer.Navigator>
+    );
+};
 
 export const AppNavigation = () => {
     return (
         <NavigationContainer>
-            <Drawer.Navigator>
-                <Drawer.Screen name="Main" component={MainScreen} />
-                <Drawer.Screen name="About" component={AboutScreen} />
-                <Drawer.Screen name="List" component={ListScreen} />
-            </Drawer.Navigator>
+            <DrawerNavigator />
         </NavigationContainer>
     );
 };
