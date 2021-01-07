2632ed41769f28cafc70fce801e35aa7ead5f20b
SDen4 <asw325@yandex.ru>
2021-01-07 22:58:39 +0500

    BackHandler for only Android - close the app by hardwareBackPress with modal window

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,20 +1,39 @@
-import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, StyleSheet, Text, Button, BackHandler, Alert } from 'react-native';
 
 import { THEME } from '../theme';
 
 export const MainScreen = ({ navigation }) => {
+    useEffect(() => {
+        const backAction = () => {
+            Alert.alert('Close the app', 'Are you sure to close the app?', [
+                {
+                    text: 'Cancel',
+                    onPress: () => null,
+                    style: 'cancel',
+                },
+                {
+                    text: 'Yes',
+                    onPress: () => BackHandler.exitApp(),
+                },
+            ]);
+            return true;
+        };
+        const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
+        return () => backHandler.remove();
+    }, []);
+
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Main Screen</Text>
             <Button
                 color={THEME.DANGER_COLOR}
-                title="Go to About"
+                title='Go to About'
                 onPress={() => navigation.push('About')}
             />
             <Button
                 color={THEME.DANGER_COLOR}
-                title="Go to List"
+                title='Go to List'
                 onPress={() => navigation.push('List')}
             />
         </View>
