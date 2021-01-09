45823894f658686d940c7f3e97d41a5c4f0df4fe
SDen4 <asw325@yandex.ru>
2021-01-09 23:58:47 +0500

    del

--- src/components/AppHeaderIcon.js
+++
@@ -1,6 +0,0 @@
-// import { HeaderButton } from 'react-navigation-header-buttons';
-// import { Entypo } from '@expo/vector-icons';
-
-// export const AppHeaderIcon = (props) => (
-//     <HeaderButton {...props} iconSize={24} color='#fff' IconComponent={Entypo} />
-// );
